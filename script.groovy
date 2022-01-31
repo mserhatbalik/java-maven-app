fba89ff8f424a60c40e55e06bc9ee8440c1aa079
mserhatbalik <47850718+mserhatbalik@users.noreply.github.com>
2022-01-31 14:34:33 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,10 @@ def buildJar() {
 def buildImage() {
   echo 'building the docker image..'
   withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-  sh 'docker build -t 209.97.182.161:8083/my-latest-app:1.0 .'
-  sh "echo $PASS | docker login -u $USER --password-stdin 209.97.182.161:8083"
-  sh 'docker push 209.97.182.161:8083/my-latest-app:1.0'
+    sh 'docker build -t 209.97.182.161:8083/my-latest-app:1.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin 209.97.182.161:8083"
+    sh 'docker push 209.97.182.161:8083/my-latest-app:1.0'
+  }
 }
 
 def deployApp() {
